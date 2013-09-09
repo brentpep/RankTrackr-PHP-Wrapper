debaf842c91ebb229e756cb9ecfb763a626c7dc8
BrentPep <brent.pepitone@gmail.com>
2013-09-09 10:04:58 -0500

    Added documentation

---
+++ README.md
@@ -0,0 +1,43 @@
+#RankTrackr PHP Wrapper
+A simple, user-friendly PHP Wrapper for RankTrackr's API
+
+## Important
+The data being pulled in via [RankTrackr API](http://users.ranktrackr.com/docs) takes a while to load if you are tracking many URLs and keywords. I HIGHLY suggest to grab the data, store it in a database and pull in the info via database.
+You will see how I did it in the wrapper.
+
+## Useage Example
+```php
+<?php
+	// Init Class
+	$RankTrackr = new RankTrackr();
+	$RankTrackr->initUpdate();
+?>
+```
+
+## Documentation
+The wrapper makes using cURL very easy. To get the RankTrackr Auth Token:
+```php
+	public function getAuthToken(){
+		$this->setRoute('api/v1/token');
+		$this->enablePostMethod();
+		
+		$postFields = array('email'=>$this->_username,'password'=>$this->_password);
+		$this->_postFields = json_encode($postFields);
+		
+		$this->createCurl();
+		$result = $this->_curlResult;
+		
+		$token = json_decode($result);
+		$this->_tokenKey = $token->access_token;
+		
+		return json_decode($result, true);
+	}
+```
+Just change `$this->setRoute('')` to the route located in the API Documentation.
+
+### Current Methods
+```php
+	getAuthToken()
+	getUrls()
+	getKeywords()
+```
--- RankTrackr.wrapper.php
+++ RankTrackr.wrapper.php
@@ -18,7 +18,28 @@
 The MIT License (MIT)
 http://opensource.org/licenses/MIT
 
-Copyright(c) 2013
+Copyright (c) 2013 Brent Pepitone
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
 */
 
 class RankTrackr{
@@ -45,6 +66,7 @@ class RankTrackr{
 	private $_followLocation = true;
 	private $_postFields	 = array();
 	
+	# Make changes to support how you want to handle the RankTrackr Data
 	public function initUpdate(){
 	    # Truncate the two DB tables
 		$this->truncateTables();
@@ -54,6 +76,7 @@ class RankTrackr{
 		
 		# Get URLs and store in DB
 		$urls = $this->getUrls();
+		$output = array();
 		foreach($urls as $url){
 			$last_processed_at = $this->isoToTimeStamp($url['last_processed_at']);
 			$created_at = $this->isoToTimeStamp($url['created_at']);
@@ -70,6 +93,7 @@ class RankTrackr{
 			}
 		}
 		
+		return $output;
 	}
 	
 	public function truncateTables(){
