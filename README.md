e1e45de188b5e48b0d741048f960a9b025c5b7c2
Brent Pepitone <brent@legnd.com>
2016-04-25 08:07:23 -0500

    v1.1 - SSL support

--- README.md
+++ README.md
@@ -20,16 +20,16 @@ The wrapper makes using cURL very easy. To get the RankTrackr Auth Token:
 	public function getAuthToken(){
 		$this->setRoute('api/v1/token');
 		$this->enablePostMethod();
-		
+
 		$postFields = array('email'=>$this->_username,'password'=>$this->_password);
 		$this->_postFields = json_encode($postFields);
-		
+
 		$this->createCurl();
 		$result = $this->_curlResult;
-		
+
 		$token = json_decode($result);
 		$this->_tokenKey = $token->access_token;
-		
+
 		return json_decode($result, true);
 	}
 ```
@@ -42,5 +42,10 @@ Just change `$this->setRoute('')` to the route located in the API Documentation.
 	function getKeywords();
 ```
 
-#### Note about updates
-I use this class quite a bit and plan on making changes as I need them, so keep an eye out on updates.
+#### Update 1.1
+The only major change here is that RankTrackr is now enforcing SSL for the user access on the subdomain 'users.ranktrackr.com', which should be used only for interface access.
+This prompted me to change around some methods to make it easy to reflect these changes. These routes have already been updated in the protected vars. The only thing you will need to update on your local wrapper is the two different methods: `$this->setApiRoute('api/v1/urls?access_token='.$this->_tokenKey);` (notice the `$this->setApiRoute()`, instead of the old `$this->setRoute`).
+
+`setBaseRoute()` will ONLY be used when generating the initial token.
+
+This was a quick fix, so report any bugs you might find. Thanks!
--- RankTrackr.wrapper.php
+++ RankTrackr.wrapper.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 /**
  * RankTrackr.wrapper.php
  *
- * Access RankTrackr API via cURL. Currently this wrapper only 
+ * Access RankTrackr API via cURL. Currently this wrapper only
  * contains methods for grabbing URLs and Keywords associated with the URLs.
  *
  * PHP version 5
@@ -10,7 +10,7 @@
  *
  * @author     Brent Pepitone <brent@legnd.com>
  * @license    The MIT License (MIT)
- * @version    1.0
+ * @version    1.1
  * @link       n/a
  */
 
@@ -43,99 +43,100 @@ OTHER DEALINGS IN THE SOFTWARE.
 */
 
 class RankTrackr{
-	
+
 	# RankTrackr Creditentials
 	private $_username 		 = 'email';
 	private $_password 		 = 'password';
 	private $_tokenKey		 = '';
-	
+
 	# (optional) MySQL Tables
-	private $_urlTable		 = 'ranktrackr_urls';
-	private $_keywordTable	 = 'ranktrackr_keywords';
-	
-	public $output			 = array();
-	
-	private $_baseRoute 	 = 'http://users.ranktrackr.com/';
-	private $_route			 = '';
-	private $_curlResult	 = '';
-	
+	private $_urlTable		 		= 'ranktrackr_urls';
+	private $_keywordTable	 	= 'ranktrackr_keywords';
+
+	public $output = array();
+
+	private $_baseRoute 	 	= 'https://users.ranktrackr.com/';
+	private $_apiEndpoint  	= 'http://api.ranktrackr.com/';
+	private $_route			 		= '';
+	private $_curlResult	 	= '';
+
 	# cURL Settings
-	private $_isPost		 = false;
-	private $_timeout		 = 30;
+	private $_isPost		 		 = false;
+	private $_timeout		 		 = 30;
 	private $_maxRedirects	 = 4;
 	private $_followLocation = true;
-	private $_postFields	 = array();
-	
+	private $_postFields	   = array();
+
 	# Make changes to support how you want to handle the RankTrackr Data
 	public function initUpdate(){
 	    # Truncate the two DB tables
 		$this->truncateTables();
-		
+
 		# Get Auth Token from RankTrackr
 		$this->getAuthToken();
-		
+
 		# Get URLs and store in DB
 		$urls = $this->getUrls();
 		$output = array();
 		foreach($urls as $url){
 			$last_processed_at = $this->isoToTimeStamp($url['last_processed_at']);
 			$created_at = $this->isoToTimeStamp($url['created_at']);
-			
+
 			# Insert Query for URLs
-			
+
 			# Get Keywords for URLs and store to DB
 			$keywords = $this->getKeywords($url['id']);
 			foreach($keywords as $keyword){
 				$last_processed_at = $this->isoToTimeStamp($keyword['last_processed_at']);
 				$created_at = $this->isoToTimeStamp($keyword['created_at']);
-				
+
 				# Insert Query for Keywords
 			}
 		}
-		
+
 		return $output;
 	}
-	
+
 	public function truncateTables(){
 		# Truncate MySQL tables before storing the new batch
 	}
-	
+
 	public function getAuthToken(){
-		$this->setRoute('api/v1/token');
+		$this->setBaseRoute('api/v1/token');
 		$this->enablePostMethod();
-		
+
 		$postFields = array('email'=>$this->_username,'password'=>$this->_password);
 		$this->_postFields = json_encode($postFields);
-		
+
 		$this->createCurl();
 		$result = $this->_curlResult;
-		
+
 		$token = json_decode($result);
 		$this->_tokenKey = $token->access_token;
-		
+
 		return json_decode($result, true);
 	}
-	
+
 	public function getUrls(){
-		$this->setRoute('api/v1/urls?access_token='.$this->_tokenKey);
+		$this->setApiRoute('api/v1/urls?access_token='.$this->_tokenKey);
 		$this->disablePostMethod();
-		
+
 		$this->createCurl();
 		$result = $this->_curlResult;
-		
+
 		return json_decode($result, true);
 	}
-	
+
 	public function getKeywords($urlId){
-		$this->setRoute('api/v1/urls/'.$urlId.'/keywords?access_token='.$this->_tokenKey);
+		$this->setApiRoute('api/v1/urls/'.$urlId.'/keywords?access_token='.$this->_tokenKey);
 		$this->disablePostMethod();
-		
+
 		$this->createCurl();
 		$result = $this->_curlResult;
-		
+
 		return json_decode($result, true);
 	}
-	
+
 	public function isoToTimeStamp($time){
 		# Convert to UNIX
 		$unix = date("U",strtotime($time));
@@ -143,40 +144,47 @@ class RankTrackr{
 		$gmt = gmdate('r',$unix);
 		# Finally convert to timestamp for database entry
 		$timestamp = date('Y-m-d H:i:s', strtotime($gmt));
-		
+
 		return $timestamp;
 	}
-	
+
 	public function setLastUpdated($time){
 		$this->_lastUpdated = $time;
 	}
 	public function getLastUpdated(){
 		return $this->_lastUpdated;
 	}
-	
-	public function setRoute($route){
-		$this->_route = $route;
+
+	public function setApiRoute($route){
+		$this->_apiEndpoint = $route;
 	}
-	public function getRoute(){
+	public function getApiRoute(){
+		return $this->_apiEndpoint . $this->_route;
+	}
+
+	public function setBaseRoute($route){
+		$this->_baseRoute = $route;
+	}
+	public function getBaseRoute(){
 		return $this->_baseRoute . $this->_route;
 	}
-	
+
 	public function disablePostMethod(){
 		# Use this for GET
 		$this->_isPost = false;
-		return $this;
+		return $this; # Allow method chaining
 	}
-	
+
 	public function postMethod(){
 		return $this->_isPost;
 	}
-	
+
 	public function enablePostMethod(){
 		# Use this for POST
 		$this->_isPost = true;
-		return $this;
+		return $this; # Allow method chaining
 	}
-	
+
 	public function curlHttpHeader(){
 		$header = array(
 			'Accept: application/json',
@@ -185,12 +193,12 @@ class RankTrackr{
 		);
 		return $header;
 	}
-	
-	public function createCurl(){
-		$route = $this->getRoute();
-		
+
+	public function createCurl($base = false){
+		$route = ($base == false ? $this->getApiRoute() : $this->getBaseRoute());
+
 		$ch = curl_init();
-		
+
 		curl_setopt($ch, CURLOPT_URL, $route);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->curlHttpHeader());
 		curl_setopt($ch, CURLOPT_TIMEOUT, $this->_timeout);
@@ -200,17 +208,17 @@ class RankTrackr{
 		curl_setopt($ch, CURLOPT_MAXREDIRS, $this->_maxRedirects);
 		curl_setopt($ch, CURLOPT_POST, $this->postMethod());
 		if($this->postMethod()==true) curl_setopt($ch, CURLOPT_POSTFIELDS, $this->_postFields);
-		
+
 		$this->_curlResult = $this->execCurl($ch);
 		$this->_curlStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		
+
 		$this->closeCurl($ch);
 	}
-	
+
 	public function execCurl($route){
 		return curl_exec($route);
 	}
-	
+
 	public function closeCurl($ch){
 		return curl_close($ch);
 	}
